9114855553dc14f9c084372be8929dc5df053c0d
xanderios <alexandreriosme@gmail.com>
2021-11-19 14:57:16 -0300

    fix(journals): journal card odd colors

--- src/components/JournalCard.tsx
+++ src/components/JournalCard.tsx
@@ -3,13 +3,9 @@ import { IJournal } from '../types/journal';
 
 interface Props extends IJournal {}
 
-export default function JournalCard({ title, type }: Props): ReactElement {
+export default function JournalCard({ title }: Props): ReactElement {
   return (
-    <div
-      className={`journal-card ${
-        type === 'public' ? 'journal-card--public' : 'journal-card--private'
-      }`}
-    >
+    <div className="journal-card">
       <span className="journal-spine"></span>
       <p className="journal-title centered font-serif text-2xl">{title}</p>
     </div>
